1898edd7960943c2447224fe8446efa653c0d51e
Pratham Das <prathamdas003@gmail.com>
2024-05-26 21:17:09 +0530

    refactor(app): routes and controlers

--- Auth_Demo/index.js
+++ Auth_Demo/index.js
@@ -1,41 +1,43 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const userMiddleware  = require("./middleware/user");
-const {readAccess,writeAccess} = require("./middleware/access");
-const authRouter = require("./routes/auth");
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const userMiddleware = require('./middleware/user')
+const { readAccess, writeAccess } = require('./middleware/access')
+const authRouter = require('./routes/auth')
+const blogRouter = require('./routes/blog')
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const port = process.env.PORT;
+const app = express()
+const port = process.env.PORT || 3000
 
 //middleware provided by Express to parse incoming JSON requests.
-app.use(express.json()); 
+app.use(express.json())
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("MongoDB is connected!");
-});
+  console.log('MongoDB is connected!')
+})
 
-app.use('/auth', authRouter);
+app.use('/auth', authRouter)
+app.use('/blog', blogRouter)
 
-app.get("/protected", userMiddleware, (req, res) => {
-  const { username } = req.user;
-  res.send(`This is a Protected Route. Welcome ${username}`);
-});
+app.get('/protected', userMiddleware, (req, res) => {
+  const { username } = req.user
+  res.send(`This is a Protected Route. Welcome ${username}`)
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
-app.get("/read", userMiddleware, readAccess, (req, res) => {
-  res.send("Read Access Granted");
-});
+app.get('/read', userMiddleware, readAccess, (req, res) => {
+  res.send('Read Access Granted')
+})
 
-app.post("/write", userMiddleware, writeAccess, (req, res) => {
-  res.send("Write Access Granted");
-});
+app.post('/write', userMiddleware, writeAccess, (req, res) => {
+  res.send('Write Access Granted')
+})
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
--- Auth_Demo/routes/blog.js
+++ Auth_Demo/routes/blog.js
@@ -2,17 +2,25 @@ const express = require('express')
 const Blog = require('../models/Blog')
 const router = express.Router()
 const verifyJWT = require('../middleware/user.js')
+const {
+  readAccess,
+  writeAccess,
+  updateAccess,
+  deleteAccess,
+} = require('../middleware/access.js')
 
-router.get('/getBlogs', verifyJWT, async (req, res) => {
+router.use(verifyJWT)
+
+const getBlogs = async (req, res) => {
   try {
     const blogs = await Blog.find({})
     return res.json({ message: 'Successful', blogs, success: true }).status(200)
   } catch (error) {
     console.error(error)
   }
-})
+}
 
-router.post('/createBlog', verifyJWT, async (req, res) => {
+const createBlog = async (req, res) => {
   try {
     const { title, content } = req.body
     if (!title || !content) {
@@ -27,9 +35,9 @@ router.post('/createBlog', verifyJWT, async (req, res) => {
   } catch (error) {
     console.error(error)
   }
-})
+}
 
-router.patch('/updateBlog', verifyJWT, async (req, res) => {
+const updateBlog = async (req, res) => {
   try {
     const { title, content } = req.body
     const { id } = req.params
@@ -42,14 +50,18 @@ router.patch('/updateBlog', verifyJWT, async (req, res) => {
 
     const updatedBlog = await Blog.findById(id)
     return res
-      .json({ message: 'successfully updated blog', success: true })
+      .json({
+        message: 'successfully updated blog',
+        success: true,
+        updatedBlog,
+      })
       .status(200)
   } catch (error) {
     console.error(error)
   }
-})
+}
 
-router.delete('/deleteBlog', verifyJWT, async (req, res) => {
+const deleteBlog = async (req, res) => {
   try {
     const { id } = req.params
 
@@ -63,4 +75,14 @@ router.delete('/deleteBlog', verifyJWT, async (req, res) => {
   } catch (error) {
     console.error(error)
   }
-})
+}
+
+router.route('/getBlogs').get(readAccess, getBlogs)
+
+router.route('/createBlog').post(writeAccess, createBlog)
+
+router.route('/updateBlog').patch(updateAccess, updateBlog)
+
+router.route('/deleteBlog').delete(deleteAccess, deleteBlog)
+
+module.exports = router
